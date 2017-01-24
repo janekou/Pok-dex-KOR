48671ea096e370a99ebe7a382d4e0919758ec4e3
Jane <Jane@SJ.local>
2017-01-24 17:55:04 -0500

    Initial Commit

---
+++ 포켓몬도감/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  포켓몬도감
+//
+//  Created by Jane on 24/01/2017.
+//  Copyright © 2017 Jane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
