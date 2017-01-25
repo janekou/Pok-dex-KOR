36449d17d4745b9cba923e52dcd11ee3df4d5560
Jane <Jane@SJ.local>
2017-01-25 16:23:43 -0500

    Korean names and PokemonDetailVC

---
+++ 포켓몬도감/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  포켓몬도감
+//
+//  Created by Jane on 24/01/2017.
+//  Copyright © 2017 Jane. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ 포켓몬도감/PokemonDetailVC.swift
@@ -0,0 +1,39 @@
+//
+//  PokemonDetailVC.swift
+//  포켓몬도감
+//
+//  Created by Jane on 25/01/2017.
+//  Copyright © 2017 Jane. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+
+    @IBOutlet weak var nameLbl: UILabel!
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLbl.text = "#" + String(pokemon.pokedexId) + " " + pokemon.name.capitalized
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
